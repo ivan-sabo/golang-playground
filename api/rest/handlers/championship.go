7f162d63e75542cbbfcefd251d2b32b93c9ab7b8
Ivan Sabo <123sabo@gmail.com>
2023-02-15 23:11:13 +0100

    swagger fix: models are finally visible, note - install dev version of swagger cli

--- api/rest/handlers/championship.go
+++ api/rest/handlers/championship.go
@@ -7,15 +7,20 @@ import (
 	"github.com/ivan-sabo/golang-playground/api/rest/models"
 )
 
-// swagger:route GET /championship championship-tag
+// swagger:route GET /championship Championship getChampionship
 // Championship holds all data relevant to a soccer league.
-// responses:
 //
-//	200: championshipResponse
+//	Produces:
+//		- application/json
+//
+//	responses:
+//		200: ChampionshipResponse
 func GetChampionship(c *gin.Context) {
-	ch := models.ChampionshipResponse{
-		Clubs: models.Clubs{
-			{Name: "NK Radnicki"},
+	ch := models.ChampionshipResponseWrapper{
+		Body: models.Championship{
+			Clubs: models.Clubs{
+				{Name: "NK Radnicki"},
+			},
 		},
 	}
 	c.JSON(http.StatusOK, ch)
--- api/rest/models/championship.go
+++ api/rest/models/championship.go
@@ -1,20 +1,22 @@
 package models
 
 // This text will appear as description of your response body.
-// swagger:response championshipResponse
+// swagger:response ChampionshipResponse
 type ChampionshipResponseWrapper struct {
-	// in:body
-	Body ChampionshipResponse
+	// in: body
+	Body Championship
 }
 
-// ChampionshipResponse represents a body of Championship reponse
-type ChampionshipResponse struct {
+// Championship represents a body of Championship reponse
+// swagger:model
+type Championship struct {
 	// Collection of clubs
 	// required: true
 	Clubs Clubs `json:"clubs"`
 }
 
 // Clubs is a collection of Club type
+
 type Clubs []Club
 
 // Club represents a football club
